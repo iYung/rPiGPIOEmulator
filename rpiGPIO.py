480378dc09c7b8cc06a9f85ad6d0882fffead03f
iYung <ivankhyung@gmail.com>
2017-09-21 19:17:40 +0000

    finished up setmode()

--- rpiGPIO.py
+++ rpiGPIO.py
@@ -8,9 +8,12 @@ BCMdic = {2:3,3:5,4:7,5:29,6:31,7:26,8:24,9:21,10:19,11:23,12:32,13:33,14:8,15:1
 
 #set mode 
 def setmode(mode):
+    global isBoard
     if (mode == BCM):
+        isBoard = False
         print("GPIO mode set to BCM")
     elif (mode == BOARD):
+        isBoard = True
         print("BOARD")
     else:
         print("WARNING: Invalid GPIO mode was selected")
